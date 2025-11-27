7902091b1b3ae2615bd13d4d050ab3f2a2022853
miyashiromc <mkmcmiyako@gmail.com>
2025-11-26 23:14:02 -0500

    guardadex

---
+++ dashboard/firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase Configuration
+export const firebaseConfig = {
+    apiKey: "AIzaSyArNK2FpOQGR3Mu-KwK27TN196a5JXDR_4",
+    authDomain: "proyecto-iot-agua.firebaseapp.com",
+    databaseURL: "https://proyecto-iot-agua-default-rtdb.firebaseio.com",
+    projectId: "proyecto-iot-agua",
+    storageBucket: "proyecto-iot-agua.firebasestorage.app",
+    messagingSenderId: "208535688741",
+    appId: "1:208535688741:web:7d3b71c410c69af4a9d1cc",
+    measurementId: "G-NP2HYVNJDP"
+};
